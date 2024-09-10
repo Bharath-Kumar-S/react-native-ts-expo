0e968c86f0e3a4f125ceaf94a2605656ccd0d90b
sridhar-palani <sridhar24062000@gmail.com>
2024-09-10 23:41:32 +0530

    Added some user input and Tried background image

--- App.tsx
+++ App.tsx
@@ -1,16 +1,20 @@
-import { StatusBar } from "expo-status-bar";
 import { useState } from "react";
-import { StyleSheet, Text, View, TextInput, Button } from "react-native";
+import { StatusBar } from "expo-status-bar";
+import { ImageBackground,StyleSheet, Text, View, TextInput, Button } from "react-native";
 import { Picker } from "./components/Picker";
+
 
 export default function App() {
   const [name, setName] = useState("");
   const [age, setAge] = useState("");
+  const [number, setNumber] = useState("");
+
+  const image = {uri:'https://legacy.reactjs.org/logo-og.png'}
 
   const handleSumbit = () => {
     //validate name and age
-    if (name === "" || age === "") {
-      alert("Please enter a name and age");
+    if (name === "" || age === "" || number === "") {
+      alert("Please enter name,age and Number");
       return;
     }
     alert(`Hello ${name} you are ${age} years old`);
@@ -18,7 +22,8 @@ export default function App() {
 
   return (
     <View style={styles.container}>
-      <Text>Hello World!</Text>
+      <ImageBackground source={image} resizeMode="cover" style={styles.image}/>
+      <Text style={styles.items}>PolarWeb</Text>
       <StatusBar style="dark" hidden />
 
       <TextInput
@@ -27,22 +32,29 @@ export default function App() {
         keyboardType="default"
         onChangeText={(text) => setName(text)}
         placeholder="Enter your name"
-        style={{ borderWidth: 1, padding: 10, margin: 10 }}
+        style={{ borderWidth: 1, padding: 10, margin: 10,width:'90%'}}
+
       />
       <TextInput
         value={age}
         keyboardType="numeric"
         onChangeText={(text) => setAge(text)}
         placeholder="Enter your age"
-        style={{ borderWidth: 1, padding: 10, margin: 10 }}
+        style={{ borderWidth: 1, padding: 10, margin: 10, width:'90%' }}
+      />
+      <TextInput
+        value={number}
+        keyboardAppearance="dark"
+        keyboardType="numeric"
+        onChangeText={(text) => setNumber(text)}
+        placeholder="Enter Your Number"
+        maxLength={10}
+        style={{ borderWidth: 1, padding: 10, margin: 10, width:'90%' }}
       />
       <Picker />
-      <Button title="Submit" onPress={handleSumbit} />
-      <Text>
-        Your name is {name} and your age is {age}
-      </Text>
+      <Button color='#000000' title="Sign Up" onPress={handleSumbit} />
     </View>
-  );
+  ); 
 }
 
 const styles = StyleSheet.create({
@@ -52,6 +64,15 @@ const styles = StyleSheet.create({
     alignItems: "center",
     justifyContent: "center",
   },
+  items:{
+    fontSize:30,
+    fontWeight:'bold',
+    paddingBottom:50
+  },
+  image:{
+    flex: 1,
+    justifyContent: 'center',
+  }
 });
 function useSate(): [any, any] {
   throw new Error("Function not implemented.");
